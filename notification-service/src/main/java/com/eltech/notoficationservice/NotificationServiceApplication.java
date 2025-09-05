54637dd36f29c6c8c8775223d9d3639ea869d227
Samuro Septima <ggg.brizs@mail.ru>
2025-09-05 19:23:29 +0300

    wow

--- notification-service/src/main/java/com/eltech/notoficationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/eltech/notoficationservice/NotificationServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotoficationServiceApplication {
+public class NotificationServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NotoficationServiceApplication.class, args);
+        SpringApplication.run(NotificationServiceApplication.class, args);
     }
 
 }
